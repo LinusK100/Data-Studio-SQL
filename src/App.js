e08478134a1f2a34a05c4fcdbe87f598e5054193
LinusK100 <li.kloppenburg@gmx.de>
2026-01-25 12:09:40 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { LanguageProvider } from './hooks/useLanguage';
 import Layout from './components/Layout/Layout';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
@@ -8,6 +8,32 @@ import Privacy from './pages/Privacy';
 import Support from './pages/Support';
 import Imprint from './pages/Imprint';
 import './App.css';
+
+// Component to handle GitHub Pages redirect from 404.html
+function RedirectHandler() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if we have a redirect path from GitHub Pages 404.html
+    // The 404.html script redirects to /?/path, so location.search will be ?/path
+    const queryString = location.search;
+    if (queryString && queryString.startsWith('?/')) {
+      // Extract the path from the query parameter
+      // Format: ?/path/to/page or ?/path&other=params
+      const pathMatch = queryString.match(/^\?\/(.+?)(?:&|$)/);
+      if (pathMatch) {
+        let path = '/' + pathMatch[1].replace(/~and~/g, '&');
+        // Clean up the path
+        path = decodeURIComponent(path);
+        // Navigate to the correct route
+        navigate(path, { replace: true });
+      }
+    }
+  }, [location, navigate]);
+
+  return null;
+}
 
 function App() {
   // Get basename from package.json homepage
@@ -18,10 +44,12 @@ function App() {
   return (
     <LanguageProvider>
       <BrowserRouter basename={basename}>
+        <RedirectHandler />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout><Home /></Layout>} />
           <Route path="/privacy" element={<Layout><Privacy /></Layout>} />
+          <Route path="/datenschutz" element={<Layout><Privacy /></Layout>} />
           <Route path="/support" element={<Layout><Support /></Layout>} />
           <Route path="/imprint" element={<Layout><Imprint /></Layout>} />
         </Routes>
